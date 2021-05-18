8acc8b41f1805fea7ce4825f858e8d3a3d0f3dd5
Oheneba Poku-Marboah <ohenebakob@hotmail.com>
2021-05-19 02:23:00 +0300

    add public use credentials; rename app;

--- constants/constants.js
+++ constants/constants.js
@@ -1,5 +1,5 @@
 //TODO: when not in dev mode, remember to uncomment this and comment out the heroku one
-//export const baseUrl = 'https://mixerlogic.herokuapp.com'
+//export const baseUrl = 'https://metropolia-mixer.herokuapp.com'
 export const baseUrl = 'http://10.0.2.2:3003'
 export const tokenKeyInSecureStore = 'token'
 export const drinkItemWidth = 150;
@@ -8,11 +8,8 @@ export const drinkItemMargin = 10;
 
 export const SIGN_IN_SCREEN = 'Sign In';
 export const WEBVIEW_SCREEN = "Guest";
-export const START_UP_SCREEN = "Mixer Logic";
+export const START_UP_SCREEN = "Metropolia Mixer";
 
-export const FIND_BY_NAME = 'FIND_BY_NAME'
-export const FIND_BY_METHOD = 'FIND_BY_METHOD'
-// const FIND_BY_INGREDIENT= "FIND_BY_INGREDIENT";
-export const FIND_BY_ALCOHOL = 'FIND_BY_ALCOHOL'
+
 
 
--- helpers/helperFunctions.js
+++ helpers/helperFunctions.js
@@ -8,7 +8,7 @@ export function getHeaderTitle(route) {
   // If the focused route is not found, we need to assume it's the initial screen
   // This can happen during if there hasn't been any navigation inside the screen
   // In our case, it's "Feed" as that's the first screen inside the navigator
-  const routeName = getFocusedRouteNameFromRoute(route) ?? 'Mixer Logic';
+  const routeName = getFocusedRouteNameFromRoute(route) ?? 'Metropolia Mixer';
 
   switch (routeName) {
     case 'Drinks':
